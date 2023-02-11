8fdb4ad0f291b98210d46c57e0c55aa76fe425d8
Gordon Diggs <gordon@gordondiggs.com>
2023-02-10 23:28:26 -0500

    Add Sentry (#11)

---
+++ config/initializers/0_sentry.rb
@@ -0,0 +1,8 @@
+if ENV["SENTRY_DSN"]
+  Sentry.init do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+    config.traces_sample_rate = 1.0
+  end
+end
