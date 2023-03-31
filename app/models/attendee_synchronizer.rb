e7232808d55d3ad43f04369c17766037185a7a97
Gordon Diggs <gordon@gordondiggs.com>
2023-03-30 20:35:32 -0400

    Band-aid fix to make it so that t-shirt orders do not count as checked-in attendees
    In the future, we should import ticket classes when creating an event,
    and use those to filter eligible tickets for giveaways

--- app/models/attendee_synchronizer.rb
+++ app/models/attendee_synchronizer.rb
@@ -6,6 +6,8 @@ class AttendeeSynchronizer
   def sync
     eventbrite_attendees.each do |eb_attendee|
       next unless eb_attendee[:checked_in]
+      # TODO Make an actual match against ticket_class_id
+      next if eb_attendee[:ticket_class_name].starts_with?("Meetup T-Shirt")
 
       attendee = Attendee.find_or_initialize_by(event_id: event.id, eventbrite_id: eb_attendee[:id])
       attendee.email = eb_attendee[:profile][:email]
