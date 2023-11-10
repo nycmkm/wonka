034abdce5d7f511479abf1020d8636c33ca24d29
Gordon Diggs <gordon@gordondiggs.com>
2023-11-10 18:30:47 -0500

    Add logo to navbar (#77)
    * Add logo to navbar
    
    * Fix rubocop error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
       "No"
     end
   end
+
+  # From https://www.writesoftwarewell.com/how-to-render-svg-icons-in-rails/
+  def render_icon(icon, classes: nil)
+    classes ||= ""
+    render "icons/#{icon}", classes:
+  end
 end
