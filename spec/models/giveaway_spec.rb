b302e09118410a8c6bf5d702a040b121313e37f6
Gordon Diggs <gordon@gordondiggs.com>
2024-07-20 16:53:54 -0400

    Separate open and drawn giveaways on the event page (#103)

--- spec/models/giveaway_spec.rb
+++ spec/models/giveaway_spec.rb
@@ -22,4 +22,28 @@ RSpec.describe Giveaway do
       expect(giveaway).to be_drawn
     end
   end
+
+  describe ".drawn" do
+    it "returns giveaways with winners" do
+      giveaway = create(:giveaway)
+
+      expect(Giveaway.drawn).not_to include(giveaway)
+
+      giveaway.winners.create!(attendee: create(:attendee, event: giveaway.event), event: giveaway.event)
+
+      expect(Giveaway.drawn).to include(giveaway)
+    end
+  end
+
+  describe ".open" do
+    it "returns giveaways without winners" do
+      giveaway = create(:giveaway)
+
+      expect(Giveaway.open).to include(giveaway)
+
+      giveaway.winners.create!(attendee: create(:attendee, event: giveaway.event), event: giveaway.event)
+
+      expect(Giveaway.open).not_to include(giveaway)
+    end
+  end
 end
